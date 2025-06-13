ea3d04e3bfab0a8ca2b287f266d5ccafa35e16da
Edin Mavric <edinmavric10@gmail.com>
2025-06-13 21:31:17 +0200

    Komponente i Kondicijalno Prikazivanje

--- my-first-react-app/README.md
+++ my-first-react-app/README.md
@@ -1,3 +1,6 @@
-Napraviti funkciju (komponentu) Profil koja ce da ima tri propa: name, hobby i quote,
+Dodati jos Form komponentu, u njoj prikazati Input-e i Button-e od danas.
 
-Prikazati tri profila u App.jsx
+Prikazati tri forme, jedna za Signin, jedna za Signup i jedna za Kontakt i sve to prikazati
+u App.jsx.
+
+Paziti da sve bude REUSABLE!!!
--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,22 +1,37 @@
-function Welcome(props) {
-  return (
-    <h1>
-      Welcome to my first React app, brother {props.name}, you are {props.age}
-    </h1>
-  );
-}
+import Button from './components/Button';
+import Input from './components/Input';
+import Link from './components/Link';
+import Welcome from './components/Welcome';
 
 function App() {
   const react = 'React';
+  const bool = true;
+
   return (
     <>
       <h1>Welcome to my first {react} app</h1>
-      <Welcome name="Mustafa" age={25} />
-      <Welcome name="Ali" age={30} />
-      <Welcome name="Zeynep" age={28} />
-      <Welcome name="Ayşe" age={22} />
-      <Welcome name="Fatma" age={35} />
-      <Welcome name="Emre" age={27} />
+      {bool && react ? (
+        <>
+          <Welcome name="Mustafa" age={25} />
+          <Welcome name="Ali" age={30} />
+          <Welcome name="Zeynep" age={28} />
+          <Welcome name="Ayşe" age={22} />
+          <Welcome name="Fatma" age={35} />
+          <Welcome name="Emre" age={27} />
+        </>
+      ) : (
+        <>
+          <Link hrefLink="https://instagram.com" targetBlank />
+
+          <Input id="input-1" label />
+          <Input id="input-2" label labelTextInput="Unesite to i to" />
+          <Input id="input-3" label />
+
+          <Button textContent="Click Me" type="button" />
+          <Button textContent="Submit" type="submit" />
+          <Button textContent="Reset" type="reset" />
+        </>
+      )}
     </>
   );
 }
