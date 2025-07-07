6e0376ac1919dca1e0778d5fde58c7a52fe2867c
Edin Mavric <edinmavric10@gmail.com>
2025-07-07 21:31:30 +0200

    8. Cas i Domaci

--- IT-Camp-React-OGs/README.md
+++ IT-Camp-React-OGs/README.md
@@ -1,73 +1,14 @@
-TEHNIČKI ZAHTEVI
-Koristi React Router (react-router-dom)
+1. Napraviti To-Do listu koja ima input za unos stavke
+i dugme za dodavanje stavke i ima listu dodatih stavki.
 
-Kreirati sledeće stranice:
+Pokusati da se napravi logika za kliknute brisanje stavke.
 
-/ → Home stranica;
+---
 
-/destinations → Lista popularnih destinacija;
+2. Napraviti formu za registraciju koja ima polja za ime,
+prezime, email i lozinku.
 
-/offers → Lista aktuelnih letnjih ponuda;
+U kartici ispod napraviti prikaz svih podataka u trenutku 
+ispisivanja istih.
 
-/about → O agenciji;
-
-Kreirati layout komponentu sa navigacijom.
-
-Navigacija treba da ima linkove ka svim stranicama (koristi <Link>).
-
-Layout treba da koristi Outlet da prikazuje sadržaj stranica.
-
-Navigacija treba da se prikazuje svuda.
-
-Na dnu svake stranice treba da bude footer sa imenom agencije.
-
-Kreirati posebne komponente za prikaz podataka.
-
-DestinationCard.jsx za prikaz jedne destinacije (prihvata name i country kao props).
-
-OfferCard.jsx za prikaz jedne ponude (prihvata title i price kao props, ali koristi i children da se prikaže dodatna informacija o kategoriji).
-
-Koristiti map() za prikaz liste podataka.
-
-Prikazati listu destinacija i ponuda korišćenjem .map().
-
-Koristiti filter() za selekciju podataka.
-
-Prikazivati samo popularne destinacije.
-
-Prikazivati samo letnje ponude.
-
-Kondicioni rendering.
-
-Ako nema rezultata (npr. ako filter ne vrati ništa), prikazati poruku: „Nema dostupnih ponuda.“
-
-export const destinations = [
-{ id: 1, name: \"Pariz\", country: \"Francuska\", popular: true },
-{ id: 2, name: \"Barselona\", country: \"Španija\", popular: false },
-{ id: 3, name: \"Rim\", country: \"Italija\", popular: true }
-];
-
-export const offers = [
-{ id: 1, title: \"Letovanje u Grčkoj\", price: 450, category: \"leto\" },
-{ id: 2, title: \"Zimovanje na Kopaoniku\", price: 600, category: \"zima\" },
-{ id: 3, title: \"Prolećni city break\", price: 300, category: \"gradovi\" }
-];
-
-==========
-
-Kada su gotovi, projekat treba da ispunjava sledeće:
-Radi bez grešaka.
-
-Navigacija vodi na prave stranice.
-
-Prikaz liste radi uz map().
-
-Prikaz samo selektovanih podataka uz filter().
-
-Komponente pravilno koriste props.
-
-Neka komponenta koristi i children.
-
-404 stranica se prikazuje za nepostojeće rute.
-
-Kod je pregledan i uredan.
+Napraviti dugme koje brise sve iz svih polja.
--- IT-Camp-React-OGs/src/App.jsx
+++ IT-Camp-React-OGs/src/App.jsx
@@ -1,19 +1,86 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Students from './pages/Students';
-import Courses from './pages/Courses';
-import About from './pages/About';
+import { useState } from 'react';
 
-export default function App() {
+const App = () => {
+  // const [state, setState] = useState(0);
+
+  // const handleMinus = () => {
+  //   setState(prevState => prevState - 1);
+  // };
+
+  // const handlePlus = () => {
+  //   setState(prevState => prevState + 1);
+  // };
+
+  // const [ime, setIme] = useState('');
+
+  // const [stavka, setStavka] = useState([]);
+
+  // const handleDodajStavku = () => {
+  //   setStavka(prevStavka => [...prevStavka, `Stavka: ${stavka.length + 1}`]);
+  // };
+
+  // const [user, setUser] = useState({ name: '', email: '' });
+
+  // function handleChangeName(event) {
+  //   setUser(prevUsers => ({ ...prevUsers, name: event.target.value }));
+  // }
+
+  // function handleChangeEmail(event) {
+  //   setUser(prevUsers => ({ ...prevUsers, email: event.target.value }));
+  // }
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="students" element={<Students />} />
-        <Route path="courses" element={<Courses />} />
-        <Route path="about" element={<About />} />
-      </Route>
-    </Routes>
+    <div></div>
+
+    // <div>
+    //   <input
+    //     type="text"
+    //     placeholder="Ime"
+    //     value={user.name}
+    //     onChange={handleChangeName}
+    //   />
+    //   <input
+    //     type="email"
+    //     placeholder="Email"
+    //     value={user.email}
+    //     onChange={handleChangeEmail}
+    //   />
+
+    //   <p>
+    //     {user.name} - {user.email}
+    //   </p>
+    // </div>
+
+    //  <div
+    //   style={{
+    //     display: 'flex',
+    //     alignItems: 'center',
+    //   }}
+    // >
+    //   <button onClick={handleMinus}>-</button>
+    //   <p style={{ margin: '0' }}>{state}</p>
+    //   <button onClick={handlePlus}>+</button>
+    // </div>
+
+    // <div>
+    // <input
+    //   type="text"
+    //   placeholder="Eg. Marko"
+    //   value={ime}
+    //   onChange={event => setIme(event.target.value)}
+    // />
+    //   <p>Ime: {ime}</p>
+    // </div>
+
+    // <div>
+    //   <button onClick={handleDodajStavku}>Dodaj</button>
+    //   <ul>
+    //     {stavka.map((stavka, index) => (
+    //       <li key={index}>{stavka}</li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
-}
+};
+
+export default App;
