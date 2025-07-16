9be60ddf4944f590b0584aaaa71f584c9ffed16c
Edin Mavric <edinmavric10@gmail.com>
2025-07-16 21:33:28 +0200

    12. Cas i Domaci

--- IT-Camp-React-OGs/README.md
+++ IT-Camp-React-OGs/README.md
@@ -1,9 +1,36 @@
-https://jsonplaceholder.typicode.com/users
+- 1: Prikaz jednog posta na osnovu ID-ja
+  Napraviti komponentu koja prikazuje jedan post na osnovu ID-ja unetog u input polje.
 
-Prikazati sve korisnike i podatke o njima u karticama.
-Korisnici moraju biti prikazani u karticama koje sadrže ime, prezime, email i adresu.
+API endpoint:
+https://jsonplaceholder.typicode.com/posts/{id}
 
-Dobro odraditi HTML I CSS deo!
+Input za ID posta
+useEffect koji poziva API svaki put kad se ID promeni
+Loading i error prikaz
+Prikaz: title, body
 
-Koristiti async/await za dohvat podataka, najbolje bi bilo i provezbati grid 
-za prikaz tih kartica, po tri u svakom redu.
+---
+
+- 2: Pretraga postova po title-u
+  Napraviti komponentu koja omogućava korisniku da unese reč i filtrira postove čiji title sadrži tu reč (klijent-side filtracija nakon fetch-a).
+
+API endpoint:
+https://jsonplaceholder.typicode.com/posts
+
+fetch svih postova samo jednom (na mount)
+Input za tekst
+Prikaz postova čiji title sadrži uneseni tekst (case-insensitive)
+Prikaz titlova postova
+
+---
+
+- 3: Prikaz komentara po ID-u posta
+  Napraviti komponentu u kojoj korisnik unosi ID posta, a aplikacija prikazuje sve komentare za taj post.
+
+API endpoint:
+https://jsonplaceholder.typicode.com/comments?postId={id}
+
+Input za unos ID posta
+Fetch komentara svaki put kad se ID promeni
+Loading i error stanje
+Prikaz komentara (name, email, body)
--- IT-Camp-React-OGs/src/App.jsx
+++ IT-Camp-React-OGs/src/App.jsx
@@ -1,94 +1,51 @@
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  // 1. ------- const [data, setData] = useState([]);
-  // const [type, setType] = useState('posts');
-
-  // useEffect(() => {
-  //   fetch(`https://jsonplaceholder.typicode.com/${type}`)
-  //     .then(res => res.json())
-  //     .then(data => setData(data));
-  // }, [type]); ------- 1.
-
-  // const [users, setUsers] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  //       const json = await res.json();
-  //       setUsers(json);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-
-  //   fetchUsers();
-  // }, []); ----- 1.
-
-  // 2. Ako input ima vise od 10 karaktera,
-  // izbaciti tekst "Prekoracili ste maksimalan broj teksta"
-
-  // const [text, setText] = useState('');
-  // const [warning, setWarning] = useState('');
-
-  // 2. ------- useEffect(() => {
-  //   if (text.length > 10) {
-  //     setWarning('Prekoracili ste maksimalan broj teksta');
-  //   } else {
-  //     setWarning('');
-  //   }
-  // }, [text]); ------- 2.
-
-  // 3.
-  // const [time, setTime] = useState(new Date());
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setTime(new Date());
-  //   }, 1000);
-
-  //   return () => {
-  //     clearInterval(interval);
-  //   };
-  // }, []);
-  // 3.
-
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [users, setUsers] = useState(null);
+  const [username, setUsername] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => setInnerWidth(window.innerWidth);
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
+    const fetchUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/?username=${username}`
+        );
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
-  }, []);
+
+    fetchUsers();
+  }, [username]);
 
   return (
     <div>
-      <p>{innerWidth}px</p>
-      {/* 3. <p>{time.toLocaleString()}</p> */}
-      {/* 2. ------- <input
+      <input
         type="text"
-        placeholder="Unesite tekst"
-        value={text}
-        onChange={e => setText(e.target.value)}
+        placeholder="Unesite postojeci username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
       />
-      <p>Duzina teksta: {text.length}</p>
-      {warning && <p style={{ color: 'red' }}>{warning}</p>} ------- 2. */}
-      {/* 1. ------- <button onClick={() => setType('posts')}>Posts</button>
-      <button onClick={() => setType('users')}>Users</button>
 
-      {data.length &&
-        data.map(item => (
-          <div
-            style={{ marginBottom: '10px', borderBottom: '1px solid black' }}
-          >
-            {item.title || item.name}
-          </div>
-        ))} ------- 1. */}
+      {isLoading ? (
+        <p>Ucitavanje...</p>
+      ) : isError ? (
+        <p>Greska!</p>
+      ) : (
+        <div>
+          {users.map(user => (
+            <p>{user.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
